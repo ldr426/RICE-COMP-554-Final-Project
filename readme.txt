61382b250306d9f2679149f0c3e7202dcc62c33e
Dingrui Lei <56751303+ldr426@users.noreply.github.com>
2022-12-13 21:42:47 -0600

    Add files via upload

---
+++ readme.txt
@@ -0,0 +1,10 @@
+tcn.py: implements TCN classes using keras
+Get_Data: Get data in real time and process accordingly
+GRU.ipynb: GRU model
+LSTM.ipynb: LSTM model
+TCN.ipynb: TCN model
+TCN-Attention: TCN model with attention mechanism added (the effect is too poor, did not participate in the comparison)
+TCN-GRU: Combination model of TCN-GRU
+TCN-LSTM: Combination model of TCN-LSTM
+Compare.ipynb: perform model comparison drawing
+Note: This code is to predict the 7-day case data in China. If you need to predict other days in other countries, simply modify the corresponding code
---
+++ tcn.py
@@ -0,0 +1,278 @@
+import inspect
+from typing import List
+from tensorflow.keras import backend as K, Model, Input, optimizers
+from tensorflow.keras import layers
+from tensorflow.keras.layers import Activation, SpatialDropout1D, Lambda
+from tensorflow.keras.layers import Layer, Conv1D, Dense, BatchNormalization, LayerNormalization
+
+
+def is_power_of_two(num: int):
+    return num != 0 and ((num & (num - 1)) == 0)
+
+
+def adjust_dilations(dilations: list):
+    if all([is_power_of_two(i) for i in dilations]):
+        return dilations
+    else:
+        new_dilations = [2 ** i for i in dilations]
+        return new_dilations
+
+
+class ResidualBlock(Layer):
+
+    def __init__(self,
+                 dilation_rate: int,
+                 nb_filters: int,
+                 kernel_size: int,
+                 padding: str,
+                 activation: str = 'relu',
+                 dropout_rate: float = 0,
+                 kernel_initializer: str = 'he_normal',
+                 use_batch_norm: bool = False,
+                 use_layer_norm: bool = False,
+                 use_weight_norm: bool = False,
+                 **kwargs):
+
+        self.dilation_rate = dilation_rate
+        self.nb_filters = nb_filters
+        self.kernel_size = kernel_size
+        self.padding = padding
+        self.activation = activation
+        self.dropout_rate = dropout_rate
+        self.use_batch_norm = use_batch_norm
+        self.use_layer_norm = use_layer_norm
+        self.use_weight_norm = use_weight_norm
+        self.kernel_initializer = kernel_initializer
+        self.layers = []
+        self.shape_match_conv = None
+        self.res_output_shape = None
+        self.final_activation = None
+
+        super(ResidualBlock, self).__init__(**kwargs)
+
+    def _build_layer(self, layer):
+        self.layers.append(layer)
+        self.layers[-1].build(self.res_output_shape)
+        self.res_output_shape = self.layers[-1].compute_output_shape(self.res_output_shape)
+
+    def build(self, input_shape):
+
+        with K.name_scope(self.name):  
+            self.layers = []
+            self.res_output_shape = input_shape
+
+            for k in range(2):  
+                name = 'conv1D_{}'.format(k)
+                with K.name_scope(name): 
+                    conv = Conv1D(
+                        filters=self.nb_filters,
+                        kernel_size=self.kernel_size,
+                        dilation_rate=self.dilation_rate,
+                        padding=self.padding,
+                        name=name,
+                        kernel_initializer=self.kernel_initializer
+                    )
+                    if self.use_weight_norm:
+                        from tensorflow_addons.layers import WeightNormalization
+                        with K.name_scope('norm_{}'.format(k)):
+                            conv = WeightNormalization(conv)
+                    self._build_layer(conv)
+
+                with K.name_scope('norm_{}'.format(k)):
+                    if self.use_batch_norm:
+                        self._build_layer(BatchNormalization())
+                    elif self.use_layer_norm:
+                        self._build_layer(LayerNormalization())
+                    elif self.use_weight_norm:
+                        pass 
+
+                with K.name_scope('act_and_dropout_{}'.format(k)):
+                    self._build_layer(Activation(self.activation, name='Act_Conv1D_{}'.format(k)))
+                    self._build_layer(SpatialDropout1D(rate=self.dropout_rate, name='SDropout_{}'.format(k)))
+
+            if self.nb_filters != input_shape[-1]:
+                name = 'matching_conv1D'
+                with K.name_scope(name):     
+                    self.shape_match_conv = Conv1D(
+                        filters=self.nb_filters,
+                        kernel_size=1,
+                        padding='same',
+                        name=name,
+                        kernel_initializer=self.kernel_initializer
+                    )
+            else:
+                name = 'matching_identity'
+                self.shape_match_conv = Lambda(lambda x: x, name=name)
+
+            with K.name_scope(name):
+                self.shape_match_conv.build(input_shape)
+                self.res_output_shape = self.shape_match_conv.compute_output_shape(input_shape)
+
+            self._build_layer(Activation(self.activation, name='Act_Conv_Blocks'))
+            self.final_activation = Activation(self.activation, name='Act_Res_Block')
+            self.final_activation.build(self.res_output_shape) 
+
+            for layer in self.layers:
+                self.__setattr__(layer.name, layer)
+            self.__setattr__(self.shape_match_conv.name, self.shape_match_conv)
+            self.__setattr__(self.final_activation.name, self.final_activation)
+
+            super(ResidualBlock, self).build(input_shape) 
+
+    def call(self, inputs, training=None, **kwargs):
+        x1 = inputs
+        for layer in self.layers:
+            training_flag = 'training' in dict(inspect.signature(layer.call).parameters)
+            x1 = layer(x1, training=training) if training_flag else layer(x1)
+        x2 = self.shape_match_conv(inputs)
+        x1_x2 = self.final_activation(layers.add([x2, x1], name='Add_Res'))
+        return [x1_x2, x1]
+
+    def compute_output_shape(self, input_shape):
+        return [self.res_output_shape, self.res_output_shape]
+
+
+class TCN(Layer):
+    def __init__(self,
+                 nb_filters=64,
+                 kernel_size=3,
+                 nb_stacks=1,
+                 dilations=(1, 2, 4, 8, 16, 32),
+                 padding='causal',
+                 use_skip_connections=True,
+                 dropout_rate=0.0,
+                 return_sequences=False,
+                 activation='relu',
+                 kernel_initializer='he_normal',
+                 use_batch_norm=False,
+                 use_layer_norm=False,
+                 use_weight_norm=False,
+                 **kwargs):
+
+        self.return_sequences = return_sequences
+        self.dropout_rate = dropout_rate
+        self.use_skip_connections = use_skip_connections
+        self.dilations = dilations
+        self.nb_stacks = nb_stacks
+        self.kernel_size = kernel_size
+        self.nb_filters = nb_filters
+        self.activation_name = activation
+        self.padding = padding
+        self.kernel_initializer = kernel_initializer
+        self.use_batch_norm = use_batch_norm
+        self.use_layer_norm = use_layer_norm
+        self.use_weight_norm = use_weight_norm
+        self.skip_connections = []
+        self.residual_blocks = []
+        self.layers_outputs = []
+        self.build_output_shape = None
+        self.slicer_layer = None 
+        self.output_slice_index = None  
+        self.padding_same_and_time_dim_unknown = False 
+
+        if self.use_batch_norm + self.use_layer_norm + self.use_weight_norm > 1:
+            raise ValueError('Only one normalization can be specified at once.')
+
+        if isinstance(self.nb_filters, list):
+            assert len(self.nb_filters) == len(self.dilations)
+            if len(set(self.nb_filters)) > 1 and self.use_skip_connections:
+                raise ValueError('Skip connections are not compatible '
+                                 'with a list of filters, unless they are all equal.')
+
+        if padding != 'causal' and padding != 'same':
+            raise ValueError("Only 'causal' or 'same' padding are compatible for this layer.")
+        super(TCN, self).__init__(**kwargs)
+
+    @property
+    def receptive_field(self):
+        return 1 + 2 * (self.kernel_size - 1) * self.nb_stacks * sum(self.dilations)
+
+    def build(self, input_shape):
+        self.build_output_shape = input_shape
+        self.residual_blocks = []
+        total_num_blocks = self.nb_stacks * len(self.dilations)
+        if not self.use_skip_connections:
+            total_num_blocks += 1 
+
+        for s in range(self.nb_stacks):
+            for i, d in enumerate(self.dilations):
+                res_block_filters = self.nb_filters[i] if isinstance(self.nb_filters, list) else self.nb_filters
+                self.residual_blocks.append(ResidualBlock(dilation_rate=d,
+                                                          nb_filters=res_block_filters,
+                                                          kernel_size=self.kernel_size,
+                                                          padding=self.padding,
+                                                          activation=self.activation_name,
+                                                          dropout_rate=self.dropout_rate,
+                                                          use_batch_norm=self.use_batch_norm,
+                                                          use_layer_norm=self.use_layer_norm,
+                                                          use_weight_norm=self.use_weight_norm,
+                                                          kernel_initializer=self.kernel_initializer,
+                                                          name='residual_block_{}'.format(len(self.residual_blocks))))
+                self.residual_blocks[-1].build(self.build_output_shape)
+                self.build_output_shape = self.residual_blocks[-1].res_output_shape
+        for layer in self.residual_blocks:
+            self.__setattr__(layer.name, layer)
+
+        self.output_slice_index = None
+        if self.padding == 'same':
+            time = self.build_output_shape.as_list()[1]
+            if time is not None:
+                self.output_slice_index = int(self.build_output_shape.as_list()[1] / 2)
+            else:
+                self.padding_same_and_time_dim_unknown = True
+
+        else:
+            self.output_slice_index = -1
+        self.slicer_layer = Lambda(lambda tt: tt[:, self.output_slice_index, :], name='Slice_Output')
+        self.slicer_layer.build(self.build_output_shape.as_list())
+
+    def compute_output_shape(self, input_shape):
+        if not self.built:
+            self.build(input_shape)
+        if not self.return_sequences:
+            batch_size = self.build_output_shape[0]
+            batch_size = batch_size.value if hasattr(batch_size, 'value') else batch_size
+            nb_filters = self.build_output_shape[-1]
+            return [batch_size, nb_filters]
+        else:
+            return [v.value if hasattr(v, 'value') else v for v in self.build_output_shape]
+
+    def call(self, inputs, training=None, **kwargs):
+        x = inputs
+        self.layers_outputs = [x]
+        self.skip_connections = []
+        for res_block in self.residual_blocks:
+            try:
+                x, skip_out = res_block(x, training=training)
+            except TypeError:  
+                x, skip_out = res_block(K.cast(x, 'float32'), training=training)
+            self.skip_connections.append(skip_out)
+            self.layers_outputs.append(x)
+
+        if self.use_skip_connections:
+            x = layers.add(self.skip_connections, name='Add_Skip_Connections')
+            self.layers_outputs.append(x)
+
+        if not self.return_sequences:
+            if self.padding_same_and_time_dim_unknown:
+                self.output_slice_index = K.shape(self.layers_outputs[-1])[1] // 2
+            x = self.slicer_layer(x)
+            self.layers_outputs.append(x)
+        return x
+
+    def get_config(self):
+        config = super(TCN, self).get_config()
+        config['nb_filters'] = self.nb_filters
+        config['kernel_size'] = self.kernel_size
+        config['nb_stacks'] = self.nb_stacks
+        config['dilations'] = self.dilations
+        config['padding'] = self.padding
+        config['use_skip_connections'] = self.use_skip_connections
+        config['dropout_rate'] = self.dropout_rate
+        config['return_sequences'] = self.return_sequences
+        config['activation'] = self.activation_name
+        config['use_batch_norm'] = self.use_batch_norm
+        config['use_layer_norm'] = self.use_layer_norm
+        config['use_weight_norm'] = self.use_weight_norm
+        config['kernel_initializer'] = self.kernel_initializer
+        return config
